8f82d89c8027a820bffb6b1f9ec7486a40ce9afb
Emilien Roussel <emilienroussel9@gmail.com>
2020-04-22 12:05:55 +0200

    framework initalization

---
+++ ziwo-sdk/ziwo_sdk.h
@@ -0,0 +1,19 @@
+//
+//  ziwo_sdk.h
+//  ziwo-sdk
+//
+//  Created by Emilien ROUSSEL on 22/04/2020.
+//  Copyright © 2020 ASWAT. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ziwo_sdk.
+FOUNDATION_EXPORT double ziwo_sdkVersionNumber;
+
+//! Project version string for ziwo_sdk.
+FOUNDATION_EXPORT const unsigned char ziwo_sdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ziwo_sdk/PublicHeader.h>
+
+
