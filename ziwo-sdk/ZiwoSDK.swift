8252cf0c6171093d27032b570f3572d1bda48252
Emilien Roussel <emilienroussel9@gmail.com>
2020-04-22 12:10:46 +0200

    unit test target initialization

---
+++ ziwo-sdk/ZiwoSDK.swift
@@ -0,0 +1,19 @@
+//
+//  ZiwoSDK.swift
+//  ziwo-sdk
+//
+//  Created by Emilien ROUSSEL on 22/04/2020.
+//  Copyright © 2020 ASWAT. All rights reserved.
+//
+
+import Foundation
+
+class ZiwoSDK {
+    
+    public init() {}
+    
+    func testHelloWorld() -> String {
+        return "hello world"
+    }
+    
+}
---
+++ ziwo_sdkTests/ziwo_sdkTests.swift
@@ -0,0 +1,19 @@
+//
+//  ziwo_sdkTests.swift
+//  ziwo_sdkTests
+//
+//  Created by Emilien ROUSSEL on 22/04/2020.
+//  Copyright © 2020 ASWAT. All rights reserved.
+//
+
+import XCTest
+@testable import ziwo_sdk
+
+class ziwo_sdkTests: XCTestCase {
+
+    func testHelloWorld() {
+        let ziwodSDK = ZiwoSDK()
+        XCTAssertEqual(ziwodSDK.testHelloWorld(), "hello world")
+    }
+
+}
